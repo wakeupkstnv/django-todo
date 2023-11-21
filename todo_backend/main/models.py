4ec2b531eaa7eb387e3844535e66e34e6cfef73d
Админ <kustanaev.tamirlan@gmail.com>
2023-11-22 00:01:13 +0600

    New Commit

--- todo_backend/main/models.py
+++ todo_backend/main/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class ToDoTask(models.Model):
     title = models.CharField('Task Name', max_length=100, default='')
     description = models.CharField('Description of task', max_length=255, default='')
